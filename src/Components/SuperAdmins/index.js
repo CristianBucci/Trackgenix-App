f3905ab5e411c0d9b0db34e5f2f4b0fcdbd8b3da
Adrián Da Silva Sánchez <ansitoxkfe@gmail.com>
2022-10-28 17:57:56 -0300

    TG-43: setting fetch to get all super admins from data base

--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -1,6 +1,17 @@
+import { useEffect, useState } from 'react';
 import styles from './super-admins.module.css';
 
 function SuperAdmins() {
+  // eslint-disable-next-line no-unused-vars
+  const [superAdmins, saveSuperAdmins] = useState([]);
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_API_URL}superAdmin/`)
+      .then((response) => response.json())
+      .then((response) => {
+        saveSuperAdmins(response.data);
+      });
+  }, []);
   return (
     <section className={styles.container}>
       <h2>SuperAdmins</h2>
