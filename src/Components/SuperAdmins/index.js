11d4b74042023095ae9f9ff70bf0b1fa27af8d7a
Adrián Da Silva Sánchez <ansitoxkfe@gmail.com>
2022-10-28 18:00:13 -0300

    TG-43: list created with all super admins from database

--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -15,6 +15,36 @@ function SuperAdmins() {
   return (
     <section className={styles.container}>
       <h2>SuperAdmins</h2>
+      <div className={styles.table}>
+        <div className={styles.column}>
+          <li className={styles.listTitle}>Name</li>
+          {superAdmins.map((superAdmin) => {
+            return (
+              <li key={superAdmin.id}>
+                {superAdmin.name} {superAdmin.lastName}
+              </li>
+            );
+          })}
+        </div>
+        <div className={styles.column}>
+          <li className={styles.listTitle}>Email</li>
+          {superAdmins.map((superAdmin) => {
+            return <li key={superAdmin.id}>{superAdmin.email}</li>;
+          })}
+        </div>
+        <div className={styles.column}>
+          <li className={styles.listTitle}>Actions</li>
+          {superAdmins.map((superAdmin) => {
+            return (
+              // eslint-disable-next-line react/jsx-key
+              <div>
+                <button key={superAdmin.id}>o</button>
+                <button key={superAdmin.id}>x</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 }
