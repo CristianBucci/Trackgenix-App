bdd13c36c9ff9bfe62a2a7cef5e57417a94c24e2
Adrián Da Silva Sánchez <ansitoxkfe@gmail.com>
2022-10-29 02:21:46 -0300

    TG-43: updated the jsx for the super admins table

--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import styles from './super-admins.module.css';
 
 function SuperAdmins() {
-  // eslint-disable-next-line no-unused-vars
   const [superAdmins, saveSuperAdmins] = useState([]);
 
   useEffect(() => {
@@ -16,34 +15,25 @@ function SuperAdmins() {
     <section className={styles.container}>
       <h2>SuperAdmins</h2>
       <div className={styles.table}>
-        <div className={styles.column}>
-          <li className={styles.listTitle}>Name</li>
-          {superAdmins.map((superAdmin) => {
-            return (
-              <li key={superAdmin.id}>
+        <div className={styles.titles}>
+          <div className={styles.data}>Name</div>
+          <div className={styles.data}>Email</div>
+          <div className={styles.actionButtons}>Actions</div>
+        </div>
+        {superAdmins.map((superAdmin) => {
+          return (
+            <div className={styles.row} key={superAdmin.id}>
+              <div className={styles.data}>
                 {superAdmin.name} {superAdmin.lastName}
-              </li>
-            );
-          })}
-        </div>
-        <div className={styles.column}>
-          <li className={styles.listTitle}>Email</li>
-          {superAdmins.map((superAdmin) => {
-            return <li key={superAdmin.id}>{superAdmin.email}</li>;
-          })}
-        </div>
-        <div className={styles.column}>
-          <li className={styles.listTitle}>Actions</li>
-          {superAdmins.map((superAdmin) => {
-            return (
-              // eslint-disable-next-line react/jsx-key
-              <div>
-                <button key={superAdmin.id}>o</button>
-                <button key={superAdmin.id}>x</button>
               </div>
-            );
-          })}
-        </div>
+              <div className={styles.data}>{superAdmin.email}</div>
+              <div className={styles.actionButtons}>
+                <button>o</button>
+                <button>x</button>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </section>
   );
