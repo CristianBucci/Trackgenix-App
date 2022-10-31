9ecd8ffe854fae039549dd45da1a393526c93c1e
Mateo Scarabino <91098568+matescarabino@users.noreply.github.com>
2022-10-31 16:51:54 -0300

    TG-45: fix modals CSS

--- src/Components/TimeSheets/Modal/index.js
+++ src/Components/TimeSheets/Modal/index.js
@@ -15,13 +15,13 @@ function Modal(props) {
         <h3 className={styles.modalTitle}>{props.title}</h3>
         <div className={styles.buttonsDiv}>
           <div>
-            <button onClick={props.closeModal} className={styles.buttonCancel}>
+            <button onClick={props.closeModal} className={styles.cancel}>
               Cancel
             </button>
           </div>
           <div>
             <button
-              className={styles.buttonConfirm}
+              className={styles.confirm}
               onClick={() => {
                 onCloseModal();
               }}
