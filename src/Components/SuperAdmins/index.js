68d56e6b25f1a90576df77898e86cb64dbc8ddf8
Adrián Da Silva Sánchez <ansitoxkfe@gmail.com>
2022-10-31 04:21:54 -0300

    TG-43: AddSuperAdmin component created: onSubmit will send a POST request and update the list.

--- src/Components/SuperAdmins/index.js
+++ src/Components/SuperAdmins/index.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import List from './List/List';
+import AddSuperAdmin from './AddSuperAdmin/AddSuperAdmin';
 import styles from './super-admins.module.css';
 
 const SuperAdmins = () => {
@@ -21,16 +22,39 @@ const SuperAdmins = () => {
         .then((response) => response.json())
         .then((response) => {
           setSuperAdminsList(response.data);
-        })
-        .catch((err) => {
-          console.log(err);
         });
     });
+  };
+
+  const addSuperAdmin = (input) => {
+    fetch(`${process.env.REACT_APP_API_URL}superAdmin/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(input)
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        alert(response.message);
+      })
+      .then(() => {
+        fetch(`${process.env.REACT_APP_API_URL}superAdmin/`)
+          .then((response) => response.json())
+          .then((response) => {
+            setSuperAdminsList(response.data);
+          });
+      });
   };
 
   return (
     <div className={styles.container}>
       <h2>SuperAdmins</h2>
+      <AddSuperAdmin
+        superAdminsList={superAdminsList}
+        setSuperAdminsList={setSuperAdminsList}
+        addSuperAdmin={addSuperAdmin}
+      />
       <List
         superAdminsList={superAdminsList}
         setSuperAdminsList={setSuperAdminsList}
