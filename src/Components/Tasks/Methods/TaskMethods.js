1288c1d97955bababcb9b1854c038e6983663c09
Maximiliano González <maxig.dev@gmail.com>
2022-11-01 20:12:24 -0300

    TG-46: Add del, post and put methods

---
+++ src/Components/Tasks/Methods/TaskMethods.js
@@ -0,0 +1,51 @@
+const del = async (id) => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/tasks/${id}`, {
+      method: 'DELETE'
+    });
+    if (!response.ok) {
+      throw new Error('ERROR: Failed to delete task');
+    }
+    alert(`Task with id ${id} delete successfully`);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+const put = async (data, id) => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/tasks/${id}`, {
+      method: 'PUT',
+      body: JSON.stringify({ description: data }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('ERROR: Failed to update task');
+    }
+    alert(`Task with id ${id} updated successfully`);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+const post = async (data) => {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/tasks`, {
+      method: 'POST',
+      body: JSON.stringify({ description: data }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    });
+    if (!response.ok) {
+      throw new Error('ERROR: Failed to create task');
+    }
+    alert(`New task ${data} created successfully`);
+  } catch (error) {
+    alert(error);
+  }
+};
+
+export { del, put, post };
--- src/Components/Tasks/index.js
+++ src/Components/Tasks/index.js
@@ -1,11 +1,43 @@
+import React, { useState, useEffect } from 'react';
 import styles from './tasks.module.css';
+import List from './TaskList/List';
+import { del, post, put } from './Methods/TaskMethods';
 
-function Tasks() {
+const Tasks = () => {
+  const [tasksList, setTasksList] = useState([]);
+
+  useEffect(async () => {
+    try {
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/tasks`);
+      const data = await res.json();
+      setTasksList(data.data);
+    } catch (error) {
+      return error;
+    }
+  }, []);
+
+  const deleteTask = async (id) => {
+    await del(id);
+    setTasksList([...tasksList.filter((task) => task._id !== id)]);
+  };
+
+  const setTask = async (data, id) => {
+    try {
+      await (id ? put(data, id) : post(data));
+      const res = await fetch(`${process.env.REACT_APP_API_URL}/tasks`);
+      const resData = await res.json();
+      setTasksList(resData.data);
+    } catch (error) {
+      return error;
+    }
+  };
+
   return (
     <section className={styles.container}>
       <h2>Tasks</h2>
+      <List tasksList={tasksList} deleteTask={deleteTask} setTask={setTask} />
     </section>
   );
-}
+};
 
 export default Tasks;
