c800e739e5c2651a45c881d1a2aea5e570a44e79
Adrián Da Silva Sánchez <ansitoxkfe@gmail.com>
2022-11-08 16:15:38 -0300

    Projects data reorganization to match the needs of the table component. Implementation of Table component complete.

--- src/Components/Projects/index.js
+++ src/Components/Projects/index.js
@@ -1,15 +1,21 @@
 import { useEffect, useState } from 'react';
 import Table from '../Shared/Table/Table';
+import { useLocation } from 'react-router-dom';
 import styles from './projects.module.css';
 
 const Projects = () => {
   const [projects, setProjects] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [showModal, setShowModal] = useState(false);
+  // eslint-disable-next-line no-unused-vars
+  const [itemId, setItemId] = useState('');
+  const location = useLocation();
 
   const getProjects = async () => {
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/projects`);
-      const data = await response.json();
-      setProjects(data.data);
+      let response = await fetch(`${process.env.REACT_APP_API_URL}/projects`);
+      response = await response.json();
+      setProjects(response.data);
     } catch (error) {
       alert('Could not GET Projects.', error);
     }
@@ -35,13 +41,43 @@ const Projects = () => {
     }
   };
 
+  const projectList = [];
+  for (let i = 0; i < projects.length; i++) {
+    const project = projects[i];
+    if (project.employees.length == 0) {
+      const newProject = {
+        ...project,
+        employees: 'N/A'
+      };
+      projectList.push(newProject);
+    } else if (project.employees[0].employeeId == null) {
+      const newProject = {
+        ...project,
+        employees: 'N/A'
+      };
+      projectList.push(newProject);
+    } else {
+      const employees = project.employees;
+      let names = [];
+      employees.map((employee) => {
+        names.push(`${employee.employeeId.name} ${employee.employeeId.lastName}`);
+      });
+      const nameList = names.join('-');
+      const newProject = {
+        ...project,
+        employees: `${nameList}`
+      };
+      projectList.push(newProject);
+    }
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.title}>
         <h2>projects</h2>
       </div>
       <Table
-        data={projects}
+        data={projectList}
         headers={[
           'Project Name',
           'Desription',
@@ -51,6 +87,7 @@ const Projects = () => {
           'Employees'
         ]}
         dataValues={['name', 'description', 'clientName', 'startDate', 'endDate', 'employees']}
+        location={location}
         modalFunction={deleteProject}
       />
     </div>
