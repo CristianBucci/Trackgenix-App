307d77963bc1662f2a07d181f19124399629b3c8
renzopimentel <renzopimentel@gmail.com>
2022-11-09 21:24:22 -0300

    TG-65: created css for datepicker and import it

--- src/Components/Shared/Datepicker/index.js
+++ src/Components/Shared/Datepicker/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import styles from '../Inputs/input.module.css';
+import styles from '../Datepicker/datepicker.module.css';
 
 const Datepicker = ({ type, label, name, value, onChange, disabled }) => {
   return (
-    <div className={styles.inputWrapper}>
+    <div className={styles.datepickerWrapper}>
       <label className={styles.label}>{label}</label>
       <input
-        className={`${disabled && styles.disabled} ${styles.input}`}
+        className={styles.input}
         type={type}
         value={value}
         name={name}
