99f3d51d16736690a3d9380d0b7140b496dc1eda
Maximiliano González <maxig.dev@gmail.com>
2022-11-09 22:43:13 -0300

    TG-63: Change file name Modal.message.js to Modal.js

---
+++ src/Components/Shared/Modal/Modal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './modal.module.css';
+
+const ModalMessage = ({ show, closeModal, modalTitle, modalContent }) => {
+  return (
+    show && (
+      <div className={styles.overlay}>
+        <div className={styles.container}>
+          <span className={styles.header}>
+            <p>{modalTitle}</p>
+            <button className={styles.modalCloseButton} onClick={() => closeModal(false)}>
+              <img src={`${process.env.PUBLIC_URL}/assets/images/close.svg`} alt="Close icon" />
+            </button>
+          </span>
+          <div className={styles.content}>
+            <span>{modalContent}</span>
+          </div>
+        </div>
+      </div>
+    )
+  );
+};
+
+export default ModalMessage;
