bcced6d8990af5236fc2d87127c789e9c2d70dc5
Nicolas Blasco <nicolas.blasco@radiumrocket.com>
2022-11-11 21:09:09 -0300

    TG-75: Redux set up

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Layout from './Components/Layout';
+import store from './redux/store';
 import './index.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers/rootReducers';
+
+const configureStore = () => {
+  const enhancer = composeWithDevTools(applyMiddleware(thunk));
+  return createStore(rootReducer, enhancer);
+};
+
+export const store = configureStore();
