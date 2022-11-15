17a72c1e74bec11cf68fe30a0af33af4e2e89d52
renzopimentel <renzopimentel@gmail.com>
2022-11-15 14:04:16 -0300

    TG-85: thunks and reducer updated, delete method added

--- src/redux/employees/reducer.js
+++ src/redux/employees/reducer.js
@@ -1,8 +1,13 @@
 import {
-  GET_EMPLOYEES_ERROR,
-  GET_EMPLOYEES_LOADING,
+  GET_EMPLOYEES_PENDING,
   GET_EMPLOYEES_SUCCESS,
-  CLOSE_MESSAGE_MODAL
+  GET_EMPLOYEES_ERROR,
+  DELETE_EMPLOYEES_PENDING,
+  DELETE_EMPLOYEES_SUCCESS,
+  DELETE_EMPLOYEES_ERROR,
+  CLOSE_MESSAGE_MODAL,
+  CONFIRM_MODAL_OPEN,
+  CONFIRM_MODAL_CLOSE
 } from './constants';
 
 const INITIAL_STATE = {
@@ -10,12 +15,13 @@ const INITIAL_STATE = {
   list: [],
   error: '',
   modalContent: { title: '', content: '' },
-  showModalMessage: false
+  showModalMessage: false,
+  showConfirmModal: false
 };
 
 const employeesReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case GET_EMPLOYEES_LOADING:
+    case GET_EMPLOYEES_PENDING:
       return {
         ...state,
         isLoading: true
@@ -34,10 +40,44 @@ const employeesReducer = (state = INITIAL_STATE, action) => {
         modalContent: { title: 'ERROR!', content: `Could not GET Employees! ${action.payload}` },
         showModalMessage: true
       };
+    case DELETE_EMPLOYEES_PENDING:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case DELETE_EMPLOYEES_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        list: [...state.list.filter((employee) => employee._id !== action.payload)]
+      };
+    case DELETE_EMPLOYEES_ERROR:
+      return {
+        ...state,
+        isLoading: false,
+        error: action.payload,
+        modalContent: { title: 'ERROR!', content: `Could not DELETE Employee! ${action.payload}` },
+        showModalMessage: true
+      };
     case CLOSE_MESSAGE_MODAL:
       return {
         ...state,
         showModalMessage: false
+      };
+    case CONFIRM_MODAL_OPEN:
+      return {
+        ...state,
+        ...state,
+        modalContent: {
+          title: 'Confirm:',
+          content: action.payload
+        },
+        showConfirmModal: true
+      };
+    case CONFIRM_MODAL_CLOSE:
+      return {
+        ...state,
+        showConfirmModal: true
       };
     default:
       return state;
--- src/redux/employees/thunks.js
+++ src/redux/employees/thunks.js
@@ -1,14 +1,43 @@
-import { getEmployeesLoading, getEmployeesSuccess, getEmployeesError } from './actions';
+import {
+  getEmployeesPending,
+  getEmployeesSuccess,
+  getEmployeesError,
+  deleteEmployeePending,
+  deleteEmployeeSuccess,
+  deleteEmployeeError
+} from './actions';
 
 export const getEmployees = () => {
   return async (dispatch) => {
-    dispatch(getEmployeesLoading());
+    dispatch(getEmployeesPending());
     try {
       const response = await fetch(`${process.env.REACT_APP_API_URL}/employees`);
       const data = await response.json();
       dispatch(getEmployeesSuccess(data.data));
     } catch (error) {
       dispatch(getEmployeesError(error.toString()));
+    }
+  };
+};
+
+export const deleteEmployee = (id) => {
+  return async (dispatch) => {
+    dispatch(deleteEmployeePending());
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/employees/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8'
+        }
+      });
+      if (response.status == 204) {
+        dispatch(deleteEmployeeSuccess(id));
+      } else {
+        const data = await response.json();
+        dispatch(deleteEmployeeError(data.data));
+      }
+    } catch (error) {
+      dispatch(deleteEmployeeError(error.toString()));
     }
   };
 };
