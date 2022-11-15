1aeca2f0f41003eb70690f01f44379de8c382408
Maximiliano González <maxig.dev@gmail.com>
2022-11-15 20:57:10 -0300

    TG-84: Update modal function

--- src/Components/Shared/Modal/ModalConfirm/index.js
+++ src/Components/Shared/Modal/ModalConfirm/index.js
@@ -1,31 +1,23 @@
 import React from 'react';
 import Buttons from '../../Button/index';
-import deleteItem from '../../../Utils/Delete/index';
 import styles from './modalConfirm.module.css';
 
-const ModalConfirm = ({ show, closeModal, modalFunction, modalTitle, modalContent, modalId }) => {
+const ModalConfirm = ({ show, onCancel, onConfirm, modalTitle, modalContent }) => {
   return (
     show && (
       <div className={styles.overlay}>
         <div className={styles.container}>
           <span className={styles.header}>
             <p>{modalTitle}</p>
-            <button className={styles.modalCloseButton} onClick={() => closeModal(false)}>
+            <button className={styles.modalCloseButton} onClick={() => onCancel()}>
               <img src={`${process.env.PUBLIC_URL}/assets/images/close.svg`} alt="Close icon" />
             </button>
           </span>
           <div className={styles.content}>
             <span>{modalContent}</span>
             <span>
-              <Buttons variant="secondary" name="Cancel" onClick={() => closeModal(false)} />
-              <Buttons
-                variant="primary"
-                name="Confirm"
-                onClick={() => {
-                  modalFunction.id ? deleteItem(modalFunction) : modalFunction(modalId);
-                  closeModal(false);
-                }}
-              />
+              <Buttons variant="secondary" name="Cancel" onClick={() => onCancel()} />
+              <Buttons variant="primary" name="Confirm" onClick={() => onConfirm()} />
             </span>
           </div>
         </div>
