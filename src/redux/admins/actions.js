f9d7c66432b8ba07db4de45c3f652be441c02022
melissabonillasellanes <melibonilla3@gmail.com>
2022-11-15 23:15:11 -0300

    TG-77: Created constants, actions, thunks and reducer

--- src/redux/admins/actions.js
+++ src/redux/admins/actions.js
@@ -2,7 +2,19 @@ import {
   GET_ADMINS_PENDING,
   GET_ADMINS_SUCCESS,
   GET_ADMINS_ERROR,
-  CLOSE_MESSAGE_MODAL
+  POST_ADMINS_PENDING,
+  POST_ADMINS_SUCCESS,
+  POST_ADMINS_ERROR,
+  UPDATE_ADMINS_PENDING,
+  UPDATE_ADMINS_SUCCESS,
+  UPDATE_ADMINS_ERROR,
+  DELETE_ADMINS_PENDING,
+  DELETE_ADMINS_SUCCESS,
+  DELETE_ADMINS_ERROR,
+  MESSAGE_MODAL_OPEN,
+  MESSAGE_MODAL_CLOSE,
+  CONFIRM_MODAL_OPEN,
+  CONFIRM_MODAL_CLOSE
 } from './constants';
 
 export const getAdminsPending = () => {
@@ -25,8 +37,88 @@ export const getAdminsError = (error) => {
   };
 };
 
+export const createAdminsPending = () => {
+  return {
+    type: POST_ADMINS_PENDING
+  };
+};
+
+export const createAdminsSuccess = (data) => {
+  return {
+    type: POST_ADMINS_SUCCESS,
+    payload: data
+  };
+};
+
+export const createAdminsError = (error) => {
+  return {
+    type: POST_ADMINS_ERROR,
+    payload: error
+  };
+};
+
+export const updateAdminsPending = () => {
+  return {
+    type: UPDATE_ADMINS_PENDING
+  };
+};
+
+export const updateAdminsSuccess = (data) => {
+  return {
+    type: UPDATE_ADMINS_SUCCESS,
+    payload: data
+  };
+};
+
+export const updateAdminsError = (error) => {
+  return {
+    type: UPDATE_ADMINS_ERROR,
+    payload: error
+  };
+};
+
+export const deleteAdminsPending = () => {
+  return {
+    type: DELETE_ADMINS_PENDING
+  };
+};
+
+export const deleteAdminsSuccess = (data) => {
+  return {
+    type: DELETE_ADMINS_SUCCESS,
+    payload: data
+  };
+};
+
+export const deleteAdminsError = (error) => {
+  return {
+    type: DELETE_ADMINS_ERROR,
+    payload: error
+  };
+};
+
+export const messageModalOpen = (content) => {
+  return {
+    type: MESSAGE_MODAL_OPEN,
+    payload: content
+  };
+};
+
 export const messageModalClose = () => {
   return {
-    type: CLOSE_MESSAGE_MODAL
+    type: MESSAGE_MODAL_CLOSE
+  };
+};
+
+export const confirmModalOpen = (content) => {
+  return {
+    type: CONFIRM_MODAL_OPEN,
+    payload: content
+  };
+};
+
+export const confirmModalClose = () => {
+  return {
+    type: CONFIRM_MODAL_CLOSE
   };
 };
