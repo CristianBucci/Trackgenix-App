6368030af704efd3fc8fd70b54e5f0db84e9b18f
Mateo Scarabino <91098568+matescarabino@users.noreply.github.com>
2022-11-19 21:15:41 -0300

    TG-100: fix PR comments

--- src/Components/Employees/Form/validations.js
+++ src/Components/Employees/Form/validations.js
@@ -6,22 +6,35 @@ export const employeeSchema = Joi.object({
     .pattern(/^[\p{L}]+$/u)
     .min(3)
     .messages({
-      'string.pattern.base': 'All characters must be letters.'
+      'string.empty': 'Name is required.',
+      'any.required': 'Name is required.',
+      'string.pattern.base': 'All characters must be letters.',
+      'string.min': 'Name minimum length is 3.'
     }),
   lastName: Joi.string()
     .required()
     .pattern(/^[\p{L}]+$/u)
     .min(3)
     .messages({
-      'string.pattern.base': 'All characters must be letters.'
+      'string.empty': 'Last Name is required.',
+      'any.required': 'Last Name is required.',
+      'string.pattern.base': 'All characters must be letters.',
+      'string.min': 'Last Name minimum length is 3.'
     }),
   email: Joi.string()
     .required()
-    .email({ tlds: { allow: false } }),
+    .email({ tlds: { allow: false } })
+    .messages({
+      'string.empty': 'Email is required.',
+      'any.required': 'Email is required.',
+      'string.pattern.base': 'Insert a valid email.'
+    }),
   password: Joi.string()
     .required()
     .pattern(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/)
     .messages({
+      'string.empty': 'Password is required.',
+      'any.required': 'Password is required.',
       'string.pattern.base': 'Must contain letters, numbers and at least 8 characters long.'
     }),
   phone: Joi.string()
@@ -30,6 +43,10 @@ export const employeeSchema = Joi.object({
     .min(9)
     .max(15)
     .messages({
-      'string.pattern.base': 'All characters must be digits.'
+      'string.empty': 'Phone is required.',
+      'any.required': 'Phone is required.',
+      'string.pattern.base': 'All characters must be digits.',
+      'string.min': 'Phone minimum length is 9.',
+      'string.max': 'Phone maximum length is 15.'
     })
 });
