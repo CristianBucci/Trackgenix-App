e37ec1e7245484160a573a6ae08c75542df80108
Mateo Scarabino <91098568+matescarabino@users.noreply.github.com>
2022-11-29 20:29:00 -0300

    TG-118: create signUp form, validations and style

--- src/Components/Auth/SignUp/index.js
+++ src/Components/Auth/SignUp/index.js
@@ -1,38 +1,155 @@
-import Buttons from 'Components/Shared/Button';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from 'helpers/firebase';
-import { mokedUsers } from 'helpers/firebase';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { confirmModalOpen, confirmModalClose, messageModalClose } from 'redux/employees/actions';
+import { createEmployee } from 'redux/employees/thunks';
 
-const SignUp = () => {
-  const { employee, admin, superAdmin } = mokedUsers;
+import ModalConfirm from 'Components/Shared/Modal/ModalConfirm';
+import ModalMessage from 'Components/Shared/Modal/ModalMessage';
+import Input from 'Components/Shared/Inputs';
+import Buttons from 'Components/Shared/Button/index';
+import styles from './signUp.module.css';
 
-  const singUpEmployee = () => signUp(employee);
+import { useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import { employeeSchema } from './validations';
 
-  const singUpAdmin = () => signUp(admin);
+function SignUp(props) {
+  const [formValues, setFormValues] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    password: '',
+    phone: ''
+  });
 
-  const singUpSuperAdmin = () => signUp(superAdmin);
+  const dispatch = useDispatch();
+  const { modalContent, showModalMessage, showConfirmModal } = useSelector(
+    (state) => state.employees
+  );
 
-  const signUp = (role) => {
-    createUserWithEmailAndPassword(auth, role.email, role.password)
-      .then((userCredential) => {
-        // Signed in
-        const user = userCredential.user;
-        alert(`User ${user.email} register successful`);
-      })
-      .catch((error) => {
-        const errorMessage = error.message;
-        alert(errorMessage);
-      });
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset
+  } = useForm({
+    mode: 'onChange',
+    resolver: joiResolver(employeeSchema)
+  });
+
+  const onConfirm = () => {
+    dispatch(createEmployee(formValues));
+    dispatch(confirmModalClose());
+  };
+
+  const onCancel = () => {
+    dispatch(confirmModalClose());
+  };
+
+  const redirect = () => {
+    props.history.push('/employees');
+  };
+
+  const onSubmit = (event) => {
+    setFormValues({
+      name: event.name,
+      lastName: event.lastName,
+      email: event.email,
+      password: event.password,
+      phone: event.phone
+    });
+
+    const content = `Are you sure you want to create a new employee?`;
+    dispatch(confirmModalOpen(content));
+  };
+
+  const modalFunction = () => {
+    modalContent.title.includes('SUCCESS') && redirect();
+    dispatch(messageModalClose());
+  };
+
+  const resetForm = () => {
+    reset(formValues);
   };
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <Buttons variant="primary" name="Sign Up Employee" onClick={singUpEmployee} />
-      <Buttons variant="secondary" name="Sign Up Admin" onClick={singUpAdmin} />
-      <Buttons variant="primary" name="Sign Up Super Admin" onClick={singUpSuperAdmin} />
-    </div>
+    <>
+      <ModalConfirm
+        show={showConfirmModal}
+        modalTitle={modalContent.title}
+        modalContent={modalContent.content}
+        onConfirm={onConfirm}
+        onCancel={onCancel}
+      />
+      <ModalMessage
+        show={showModalMessage}
+        modalTitle={modalContent.title}
+        modalContent={modalContent.content}
+        modalFunction={modalFunction}
+      />
+      <div className={styles.container}>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <h2>Create Employee</h2>
+          <Input
+            register={register}
+            label={'Name'}
+            name="name"
+            type="text"
+            error={errors.name?.message}
+            placeholder={'Name'}
+          />
+          <Input
+            register={register}
+            label={'Last Name'}
+            name="lastName"
+            type="text"
+            error={errors.lastName?.message}
+            placeholder={'Last Name'}
+          />
+          <Input
+            register={register}
+            label={'Email'}
+            name="email"
+            type="text"
+            error={errors.email?.message}
+            placeholder={'Email'}
+          />
+          <Input
+            register={register}
+            label={'Password'}
+            name="password"
+            type="password"
+            error={errors.password?.message}
+            placeholder={'Password'}
+          />
+          <Input
+            register={register}
+            label={'Repeat Password'}
+            name="repeatPassword"
+            type="repeatPassword"
+            error={errors.repeatPassword?.message}
+            placeholder={'Password'}
+          />
+          <Input
+            register={register}
+            label={'Phone'}
+            name="phone"
+            type="text"
+            error={errors.phone?.message}
+            placeholder={'Phone'}
+          />
+          <div>
+            <Link to={'/employees'}>
+              <Buttons variant="secondary" name="Cancel" />
+            </Link>
+            <Buttons type="button" variant="secondary" name="Reset" onClick={() => resetForm()} />
+            <Buttons type="submit" variant="primary" name="Confirm" />
+          </div>
+        </form>
+      </div>
+    </>
   );
-};
+}
 
 export default SignUp;
