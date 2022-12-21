154de8ac4b46a761c444717589d7989bbce37ee5
julianpiaggio <julianpiaggio@gmail.com>
2022-12-21 16:48:02 -0300

    TG-120: implement automation test e2e

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,42 +1,80 @@
+class AdminPage{
 
+  get inputName() {
+    return $('#root > div > div > form > div:nth-child(2) > input');
+  }
 
-const Page = require('./page');
+  get inputLastName() {
+    return $('#root > div > div > form > div:nth-child(3) > input');
+  }
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
-class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    get inputUsername () {
-        return $('#username');
-    }
+  get inputEmail() {
+    return $('#root > div > div > form > div:nth-child(4) > input');
+  }
 
-    get inputPassword () {
-        return $('#password');
-    }
+  get inputPassword() {
+    return $('#root > div > div > form > div:nth-child(5) > input');
+  }
 
-    get btnSubmit () {
-        return $('button[type="submit"]');
-    }
+  get titleAdmin () {
+    return $('#root > div > div > div.admins_title__DTti9 > h2')
+  }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
-    async login (username, password) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
-    }
+  get btnCreateAdmin () {
+    return $('#root > div > div > div.Table_container__L88FC > div > a')
+  }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
-    open () {
-        return super.open('login');
-    }
+  get titleCreateAdmin () {
+    return $('.form_cardTitle__OrBpx')
+  }
+
+  get btnConfirmCreateAdmin () {
+    return $('.button_primary__Z2ahL')
+  }
+
+  get modalConfirmCreateAdmin () {
+    return $('.modalConfirm_overlay__hepSv')
+  }
+
+  get btnModalConfirmCreateAdmin () {
+    return $('#root > div > div.modalConfirm_overlay__hepSv > div > div > span:nth-child(2) > button.button_primary__Z2ahL')
+  }
+
+  get modalCreateAdminSuccess () {
+    return $('.modalMessage_overlay__6NYRk')
+  }
+
+  get modalCreateAdminSuccessText () {
+    return $('#root > div > div.modalMessage_overlay__6NYRk > div > div > span')
+  }
+
+  get btnCloseModalCreateAdminSuccess () {
+    return $('#root > div > div.modalMessage_overlay__6NYRk > div > span > button')
+  }
+
+  get listAdmins () {
+    return $('#root > div > div > div.Table_container__L88FC > table > tbody > tr:last-child')
+  }
+
+  get nameAdminCreated () {
+    return $('#root > div > div > div.Table_container__L88FC > table > tbody > tr:nth-child(12) > td:nth-child(1)')
+  }
+
+  get lastNameAdminCreated () {
+    return $('#root > div > div > div.Table_container__L88FC > table > tbody > tr:nth-child(12) > td:nth-child(2)')
+  }
+
+  get emailAdminCreated () {
+    return $('#root > div > div > div.Table_container__L88FC > table > tbody > tr:nth-child(11) > td:nth-child(3)')
+  }
+
+  async create(name, lastName, email, password) {
+    await this.inputName.setValue(name);
+    await this.inputLastName.setValue(lastName);
+    await this.inputEmail.setValue(email)
+    await this.inputPassword.setValue(password);
+  }
+
 }
 
-module.exports = new LoginPage();
+module.exports = new AdminPage();
--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,15 +1,25 @@
-const LoginPage = require('../pageobjects/login.page');
-const SecurePage = require('../pageobjects/secure.page');
+const AdminPage = require('../pageobjects/login.page');
 
 describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await LoginPage.open();
-
-        await LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        await expect(SecurePage.flashAlert).toBeExisting();
-        await expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+    beforeAll('Navigation to URL', () => {
+        browser.url('https://ayom-a-trackgenix-app.vercel.app/admins');
+    })
+    it('should create,edit and delete admins', async () => {
+      await expect(AdminPage.titleAdmin).toBeDisplayed();
+      await AdminPage.btnCreateAdmin.click();
+      await expect(browser).toHaveUrl('https://ayom-a-trackgenix-app.vercel.app/admins/form');
+      await expect(AdminPage.titleCreateAdmin).toBeDisplayed();
+      await AdminPage.create('juan' , 'perez', 'juan@perez.com', 'juan1234');
+      await AdminPage.btnConfirmCreateAdmin.click();
+      await expect(AdminPage.modalConfirmCreateAdmin).toBeDisplayed();
+      await AdminPage.btnModalConfirmCreateAdmin.click();
+      await expect(AdminPage.modalCreateAdminSuccess).toBeDisplayed();
+      await expect(AdminPage.modalCreateAdminSuccessText).toHaveText('Admins Successfully CREATED');
+      await AdminPage.btnCloseModalCreateAdminSuccess.click();
+      await expect(browser).toHaveUrl('https://ayom-a-trackgenix-app.vercel.app/admins');
+      await expect(AdminPage.listAdmins).toBeDisplayed();
+      await expect(AdminPage.nameAdminCreated).toHaveText('juan');
+      await expect(AdminPage.lastNameAdminCreated).toHaveText('perez');
+      await expect(AdminPage.emailAdminCreated).toHaveText('juan@perez.com');
     });
 });
-
-
